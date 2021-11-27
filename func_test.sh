9399e8499a51ff80871280754b1afadb1775e201
Sophia <devlinsofficial@gmail.com>
2021-11-27 12:59:55 -0500

    Closes #2

--- func_test.sh
+++ func_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sophia Devlin
 
-source ./functions.sh
+source functions.sh
 
 test_construct_number () {
 	echo "Should be 1: $(construct_number 1)"
@@ -11,6 +11,11 @@ test_construct_number () {
 	echo "Should be 20: $(construct_number 20)"
 	echo "Should be 27: $(construct_number 27)"
 	echo "Should be 99: $(construct_number 99)"
+	echo "Should be 100: $(construct_number 100)"
+	echo "Should be 201: $(construct_number 201)"
+	echo "Should be 310: $(construct_number 310)"
+	echo "Should be 515: $(construct_number 515)"
+	echo "Should be 420: $(construct_number 420)"
 }
 
 
--- functions.sh
+++ functions.sh
@@ -1,92 +1,109 @@
 #!/bin/bash 
 # Sophia Devlin 11/18/21
 
-construct_number () {
-	digits=""
-	number=$1
-	first_digit=$(($number/10))
-	second_digit=$(($number%10))
-
-	case $first_digit in
-		2)
-		digits+="twenty"
-		;;
-
-		3)
-		digits+="thirty"
-		;;
-
-		4)
-		digits+="fourty"
-		;;
-
-		5)
-		digits+="fifty"
-		;;
-
-		6)
-		digits+="sixty"
-		;;
-
-		7)
-		digits+="seventy"
-		;;
-
-		8)
-		digits+="eighty"
-		;;
-
-		9)
-		digits+="ninety"
-		;;
-	esac
-	case $second_digit in 
+construct_number_helper () {
+	digit=$1
+	tens_digit=$2
+	result=""
+	case $digit in 
 		0)
-		if [ $first_digit -eq 1 ]; then digits+="ten"; fi
+		if [ $tens_digit -eq 1 ]; then result+="ten"; fi
 		;;
 		
 		1)
-		if [ $first_digit == 1 ]; then digits+="eleven"; else digits+=" one"; fi
+		if [ $tens_digit -eq 1 ]; then result+="eleven"; else result+=" one"; fi
 		;;
 
 		2)
-		if [ $first_digit == 1 ]; then digits+="twelve"; else digits+=" two"; fi
+		if [ $tens_digit -eq 1 ]; then result+="twelve"; else result+=" two"; fi
 		;;	
 		
 		3)
-		if [ $first_digit == 1 ]; then digits+="thir"; else digits+=" three"; fi
+		if [ $tens_digit -eq 1 ]; then result+="thir"; else result+=" three"; fi
 		;;
 
 		4)
-		digits+=" four"
+		result+=" four"
 		;;
 
 		5)
-		if [ $first_digit == 1 ]; then digits+="fif"; digits+=" four"; fi
+		if [ $tens_digit -eq 1 ]; then result+="fif"; else result+=" five"; fi
 		;;
 
 		6)
-		digits+=" six"
+		result+=" six"
 		;;
 
 		7)
-		digits+=" seven"
+		result+=" seven"
 		;;
 
 		8)
-		digits+=" eight"
+		result+=" eight"
 		;;
 
 		9)
-		digits+=" nine"
+		result+=" nine"
 		;;
 	esac
-	if [ $first_digit == 1 ]; then
-		if [ $second_digit -ge 3 ]; then
-			digits+="teen"
-		fi
+	echo $result
+}
+
+construct_number () {
+	result="" #This will be replaced when script changes to array of audio file names
+	number=$1
+	first_digit=$(($number/100))
+	second_digit=$(($(($number%100))/10))
+	third_digit=$(($number%10))
+
+	if [ $first_digit -gt 0 ]; then
+		result+=$(construct_number_helper $first_digit 0)
+		result+="hundred"
 	fi
 
+	case $second_digit in
+		2)
+		result+="twenty"
+		;;
 
-	echo $digits
+		3)
+		result+="thirty"
+		;;
+
+		4)
+		result+="fourty"
+		;;
+
+		5)
+		result+="fifty"
+		;;
+
+		6)
+		result+="sixty"
+		;;
+
+		7)
+		result+="seventy"
+		;;
+
+		8)
+		result+="eighty"
+		;;
+
+		9)
+		result+="ninety"
+		;;
+	esac
+
+	if [ $third_digit -ge 0 ]; then
+		result+=$(construct_number_helper $third_digit $second_digit) 
+	fi
+
+	if [ $second_digit -eq 1 ]; then
+		if [ $third_digit -ge 3 ]; then
+			result+="teen"
+		fi
+	fi
+	
+	echo $result
 }
