6e645f6a0a571eb7922458c09b1646cc0a2ce980
Sophia <devlinsofficial@gmail.com>
2021-11-30 21:37:56 -0500

    Created play_number function

--- functions.sh
+++ functions.sh
@@ -1,25 +1,27 @@
 #!/bin/bash 
 # Sophia Devlin 11/18/21
 
+audio_filepath="audio_files/"
+
 construct_number_helper () {
 	digit=$1
 	tens_digit=$2
 	result=""
 	case $digit in 
 		0)
-		if [ $tens_digit -eq 1 ]; then result+="ten"; fi
+		if [ $tens_digit -eq 1 ]; then result+=" ten"; fi
 		;;
 		
 		1)
-		if [ $tens_digit -eq 1 ]; then result+="eleven"; else result+=" one"; fi
+		if [ $tens_digit -eq 1 ]; then result+=" eleven"; else result+=" one"; fi
 		;;
 
 		2)
-		if [ $tens_digit -eq 1 ]; then result+="twelve"; else result+=" two"; fi
+		if [ $tens_digit -eq 1 ]; then result+=" twelve"; else result+=" two"; fi
 		;;	
 		
 		3)
-		if [ $tens_digit -eq 1 ]; then result+="thir"; else result+=" three"; fi
+		if [ $tens_digit -eq 1 ]; then result+=" thir"; else result+=" three"; fi
 		;;
 
 		4)
@@ -27,7 +29,7 @@ construct_number_helper () {
 		;;
 
 		5)
-		if [ $tens_digit -eq 1 ]; then result+="fif"; else result+=" five"; fi
+		if [ $tens_digit -eq 1 ]; then result+=" fif"; else result+=" five"; fi
 		;;
 
 		6)
@@ -46,64 +48,74 @@ construct_number_helper () {
 		result+=" nine"
 		;;
 	esac
-	echo $result
+	echo "$result"
 }
 
 construct_number () {
-	result="" #This will be replaced when script changes to array of audio file names
 	number=$1
+	result=""
 	first_digit=$(($number/100))
 	second_digit=$(($(($number%100))/10))
 	third_digit=$(($number%10))
 
 	if [ $first_digit -gt 0 ]; then
-		result+=$(construct_number_helper $first_digit 0)
-		result+="hundred"
+		result+="$(construct_number_helper $first_digit 0)"
+		result+=" hundred"
 	fi
 
 	case $second_digit in
 		2)
-		result+="twenty"
+		result+=" twenty"
 		;;
 
 		3)
-		result+="thirty"
+		result+=" thirty"
 		;;
 
 		4)
-		result+="fourty"
+		result+=" fourty"
 		;;
 
 		5)
-		result+="fifty"
+		result+=" fifty"
 		;;
 
 		6)
-		result+="sixty"
+		result+=" sixty"
 		;;
 
 		7)
-		result+="seventy"
+		result+=" seventy"
 		;;
 
 		8)
-		result+="eighty"
+		result+=" eighty"
 		;;
 
 		9)
-		result+="ninety"
+		result+=" ninety"
 		;;
 	esac
 
 	if [ $third_digit -ge 0 ]; then
-		result+=$(construct_number_helper $third_digit $second_digit) 
+		result+="$(construct_number_helper $third_digit $second_digit)" 
 	fi
 
 	if [ $second_digit -eq 1 ]; then
 		if [ $third_digit -ge 3 ]; then
-			result+="teen"
+			result+=" teen"
 		fi
 	fi
 	
-	echo $result
+	echo $result #A list of the audio files needed
+}
+
+play_number () {
+	num_string=$1 #A space separated list of the file names without extensions
+	num_array=( $num_string )
+	for elem in "${num_array[@]}"
+	do
+		echo "loop"
+		aplay ${audio_filepath}${elem}.wav
+	done
 }
